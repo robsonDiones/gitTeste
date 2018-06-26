16c4cca604af20ac78ebe6ab2a6e95c8eb2fe7ab
robsonDiones <dionesrobson49@gmail.com>
2018-06-26 16:14:53 -0300

    Adicinado por Diones Robson criação de uma nova classe Pessoa

---
+++ JavaApplication7/src/javaapplication7/Pessoa.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Diones Robson
+ */
+public class Pessoa {
+    public String nome;
+    public int idade;
+}

14ec1dc0e7076fa6618029f2f73d1efaf8475cf9
robsonDiones <dionesrobson49@gmail.com>
2018-06-26 16:21:32 -0300

    qualquer coisa

--- JavaApplication7/src/javaapplication7/Pessoa.java
+++ JavaApplication7/src/javaapplication7/Pessoa.java
@@ -12,4 +12,14 @@ package javaapplication7;
 public class Pessoa {
     public String nome;
     public int idade;
+    public double altura;
+    public int telefone;
+    
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    
+    
 }
